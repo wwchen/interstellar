acb42f9d56354a36b313f7eca093844462f10ba7
William Chen <williamchen16@gmail.com>
2015-08-03 01:26:31 -0700

    Refactor and added a database component to keep track of what reviews have been inserted already

--- main.py
+++ main.py
@@ -6,112 +6,79 @@ import os
 import time
 import csv
 import codecs
+import sqlite3
+import re
 
+DB_FILE = "database.sqlite3"
 CONFIG_FILE = "secrets/secrets.conf"
 CONFIG_KEY = "interstellar"
+ENCODING = "UTF_16_LE"
 BLACK_STAR = u'\u2605'
 WHITE_STAR = u'\u2606'
 
+
+def get_time(epoch_time, fmt="%b %d %H:%M"):
+    epoch_time /= 1000 # time in millis
+    return time.strftime(fmt, time.localtime(epoch_time))
+
+
+def get_rating_stars(stars, max_stars=5):
+    return BLACK_STAR * stars + WHITE_STAR * (max_stars - stars)
+
+
 class Review:
-    def __init__(self, row):
-        if not isinstance(row, list) or len(row) != 15:
+    def __init__(self, cols):
+        if not isinstance(cols, list) or len(cols) != 15:
             raise ValueError
-        row = [r.strip() for r in row]
-        self.package_name        = row[0]
-        self.app_version         = row[1]
-        self.review_lang         = row[2]
-        self.device_type         = row[3]
-        self.review_date         = row[4]
-        self.review_epoch        = row[5]
-        self.review_update_date  = row[6]
-        self.review_update_epoch = row[7]
-        self.review_rating       = row[8]
-        self.review_title        = row[9]
-        self.review_text         = row[10]
-        self.dev_reply_date      = row[11]
-        self.dev_reply_epoch     = row[12]
-        self.dev_reply_text      = row[13]
-        self.review_link         = row[14]
+        self.data = {}
+        self.id = None
+        self.col_headers = [
+            'package_name',
+            'app_version',
+            'review_lang',
+            'device_type',
+            'review_date',
+            'review_epoch',
+            'review_update_date',
+            'review_update_epoch',
+            'review_rating',
+            'review_title',
+            'review_text',
+            'dev_reply_date',
+            'dev_reply_epoch',
+            'dev_reply_text',
+            'review_link'
+        ]
+        for i, header in enumerate(self.col_headers):
+            value = cols[i].strip()
+            try:
+                value = long(value)
+            except ValueError:
+                value.encode(ENCODING)
+            finally:
+                self.data[header] = value
+                setattr(self, header, value)
+        try:
+            self.id = re.search('revid=(gp:[a-zA-Z0-9-_]*)', self.review_link).group(1)
+            assert isinstance(self.id, str) and self.id
+        except AttributeError:
+            print self.review_link
 
     def __str__(self):
+        posted_time = self.review_update_epoch or self.review_epoch
         edited_string = " (edited)" if self.review_update_epoch > self.review_epoch else ""
-        posted_time = self.get_review_update_date() or self.get_review_date()
         replied_string = " (replied)" if self.dev_reply_date else ""
 
-        print self.get_rating_stars()
+        print get_rating_stars(self.review_rating)
         print "V{}, posted on {}{}{}".format(
             self.app_version if self.app_version else "?",
-            posted_time,
+            get_time(posted_time),
             edited_string,
             replied_string)
         print "*{}* {}".format(self.review_title, self.review_text)
         return ""
 
-    def get_app_version(self, prepend = "V"):
-        if self.app_version:
-            return "{}{}".format(prepend, self.app_version)
-        return ""
-
-    def get_language(self):
-        return self.review_lang
-
-    def get_device_type(self):
-        return self.device_type
-
     ###
-
-    def get_review_epoch(self):
-        if self.review_epoch:
-            return long(self.review_epoch)
-        return None
-
-    def get_review_update_epoch(self):
-        if self.review_update_epoch:
-            return long(self.review_update_epoch)
-        return None
-
-    def get_dev_reply_epoch(self):
-        if self.dev_reply_epoch:
-            return long(self.dev_reply_epoch)
-        return None
-
-    def get_review_date(self, fmt="%b %d %H:%M"):
-        if self.review_epoch:
-            return time.strftime(fmt, time.localtime(self.get_review_epoch()))
-        return ""
-
-    def get_review_update_date(self, fmt="%b %d %H:%M"):
-        if self.review_update_epoch:
-            return time.strftime(fmt, time.localtime(self.get_review_update_epoch()))
-        return ""
-
-    def get_dev_reply_date(self, fmt="%b %d %H:%M"):
-        if self.dev_reply_epoch:
-            return time.strftime(fmt, time.localtime(self.get_dev_reply_epoch()))
-        return ""
-
-    ###
-
-    def get_review_text(self):
-        if self.review_text:
-            return self.review_text
-        return ""
-
-    def get_review_title(self):
-        if self.review_title:
-            return self.review_title
-        return ""
-
-    def get_review_link(self):
-        if self.review_link:
-            return self.review_link
-        return ""
-
-    def get_rating(self):
-        return int(self.review_rating)
-
-    def get_rating_stars(self):
-        return BLACK_STAR * self.get_rating() + WHITE_STAR * (5 - self.get_rating())
 
 class ReviewStatistics:
     def __init__(self, reviews):
@@ -121,10 +88,13 @@ class ReviewStatistics:
         self._reviews.append(review)
 
     def process(self):
+        if not len(reviews):
+            return
         stars = 0
         notable_reviews = []
         for review in self._reviews:
-            stars += review.get_rating()
+            assert isinstance(review.review_rating, long)
+            stars += review.review_rating
             if self._is_review_notable(review):
                 notable_reviews.append(review)
         self._avg_stars = float(stars) / len(self._reviews)
@@ -137,20 +107,70 @@ class ReviewStatistics:
         return False
 
     def __str__(self):
+        if not self._reviews:
+            return "No reviews accumulated"
         self.process()
         count = 4
-        self._notable_reviews.sort(key=lambda r: r.get_review_update_epoch())
+        self._notable_reviews.sort(key=lambda r: r.review_update_epoch)
         for review in self._notable_reviews:
             if count > 0:
                 print review
             else:
                 break
             count -= 1
-        return "====\nOverall stats: %0.2f stars" % (self._avg_stars)
+        return "Overall stats: %0.2f stars" % (self._avg_stars)
 
 
+class ReviewDb:
+    def __init__(self):
+        self.db_conn = sqlite3.connect(DB_FILE)
+        self.db_cursor = self.db_conn.cursor()
+        self.table = "reviews"
+        self.cols = {
+            'id':                  'TEXT',
+            'package_name':        'TEXT',
+            'app_version':         'TEXT',
+            'review_lang':         'TEXT',
+            'device_type':         'TEXT',
+            'review_epoch':        'INTEGER',
+            'review_update_epoch': 'INTEGER',
+            'review_rating':       'INTEGER',
+            'review_title':        'TEXT',
+            'review_text':         'TEXT',
+            'dev_reply_epoch':     'INTEGER',
+            'dev_reply_text':      'TEXT',
+            'review_link':         'TEXT'
+        }
+        self.create_table(self.cols)
+
+    def create_table(self, columns):
+        col_descriptions = [ "%s %s" % (col_name, type) for col_name, type in columns.iteritems()]
+        self.db_cursor.execute('CREATE TABLE IF NOT EXISTS {} ({})'.format(self.table, ','.join(col_descriptions)))
+
+    def row_exists(self, review):
+        assert isinstance(review, Review)
+        self.db_cursor.execute('SELECT * from {} WHERE id={}'.format(self.table, review.id))
+        return self.db_cursor.rowcount > 0
+
+    def save(self, review):
+        assert isinstance(review, Review)
+        params = {}
+        value_keys = []
+        named_placeholder = []
+        for col in self.cols.iterkeys():
+            params[col] = getattr(review, col)
+            value_keys.append(col)
+            named_placeholder.append(':%s' % col)
+        sql = 'INSERT INTO {} ({}) VALUES ({})'.format(self.table, ','.join(value_keys), ','.join(named_placeholder))
+        self.db_cursor.execute(sql, params)
+        self.db_conn.commit()
+
+    def close(self):
+        self.db_conn.commit()
+        self.db_conn.close()
 
 if __name__ == '__main__':
+    db = ReviewDb()
     config = ConfigParser.ConfigParser()
     config.read(CONFIG_FILE)
     slack_webhook_url = config.get(CONFIG_KEY, "slack")
@@ -188,14 +208,17 @@ if __name__ == '__main__':
     csvfile = open(review_filename, "rb").read()
     csvfile = csvfile.decode('utf_16_le').encode('utf-8').split("\n")
     csvreader = csv.reader(csvfile)
-    csvreader.next() # skip the header
+    csvreader.next()  # skip the header
     count = 0
     reviews = []
     for row in csvreader:
         try:
-            reviews.append(Review(row))
+            review = Review(row)
+            reviews.append(review)
+            db.save(review)
             count += 1
         except ValueError:
             pass
+    print "====="
     print ReviewStatistics(reviews)
     print "Processed %d ratings" % count
