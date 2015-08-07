141e08f03a14dd365364fb89448a23a32719559e
William Chen <williamchen16@gmail.com>
2015-08-07 11:40:32 -0700

    Helper function

---
+++ process_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[[ -f database.sqlite3 ]] && rm database.sqlite3
+for i in reviews/*; do
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "Processing $i"
+  ./main.py "$i"
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+done
